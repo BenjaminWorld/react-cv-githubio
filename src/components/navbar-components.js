f0a5f7408b06cf4c3c7fe508ed32cddee35d65a8
BenjaminWorld <benjiman0125@hotmail.com>
2021-11-06 03:53:48 +0800

    init project
    fixed

--- src/components/navbar-components.js
+++ src/components/navbar-components.js
@@ -1,24 +1,26 @@
+import { Router, Route, Link } from 'react-router-dom'
+
 function Navbar() {
     return (
       <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <Link class="navbar-brand" to="#">Navbar</Link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <Link class="nav-link active" aria-current="page" to="/">Home</Link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/Education">Education</a>
+            <Link class="nav-link" to="/Education">Education</Link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/WorkExperience">Work Experience</a>
+            <Link class="nav-link" to="/WorkExperience">Work Experience</Link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/Skills">Skills</a>
+            <Link class="nav-link" to="/Skills">Skills</Link>
           </li>
         </ul>
       </div>
